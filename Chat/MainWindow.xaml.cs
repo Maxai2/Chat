14b12efe1b81fd36c0c014fb92588eae738316a0
Хамелеон <maxali946@gmail.com>
2018-03-12 18:12:17 +0400

    50%

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//----------------------------------------------------------------------------
 namespace Chat
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window 
+    public partial class MainWindow : Window
     {
+        ObservableCollection<object> collection = new ObservableCollection<object>(); 
+
         public MainWindow()
         {
             InitializeComponent();
+            lbMessages.Items = collection;
         }
+        //----------------------------------------------------------------------------
+        void SendMessage()
+        {
+            if (tbUser.Text != String.Empty)
+            {
+                collection.Add(new ListBoxItem()
+                {
+                    Content = new Border()
+                    {
+                        BorderThickness = new Thickness(2),
+                        CornerRadius = new CornerRadius(25),
+                        Child = new TextBlock
+                        {
+                            Text = tbUser.Text,
+                            TextWrapping = TextWrapping.Wrap
+                        }
+                    }
+                });
 
+                lbMessages.ScrollIntoView(lbMessages.Items.Cast<ListBoxItem>().Last());
+                tbUser.Clear();
+            }
+        }
+        //----------------------------------------------------------------------------
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SendMessage();
+        }
+        //----------------------------------------------------------------------------
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            SendMessage();
         }
     }
 }
+//----------------------------------------------------------------------------
