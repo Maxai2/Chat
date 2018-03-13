a3a2136c149e49ec4a1c9ef97415571a6c8c185e
Хамелеон <maxali946@gmail.com>
2018-03-13 16:30:34 +0400

    90%

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,37 +22,68 @@ namespace Chat
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<object> collection = new ObservableCollection<object>(); 
+        ObservableCollection<object> collection = new ObservableCollection<object>();
 
+        Dictionary<bool, SolidColorBrush> color;
+        Dictionary<bool, HorizontalAlignment> alignment;
+
+
+        bool rightLeft = false;
+        string path = "Messages.txt";
+
+        //----------------------------------------------------------------------------
         public MainWindow()
         {
             InitializeComponent();
             lbMessages.ItemsSource = collection;
+
+            color = new Dictionary<bool, SolidColorBrush>()
+            {
+                { false, new SolidColorBrush(Colors.White)},
+                { true, new SolidColorBrush(Color.FromRgb(220, 248, 198))}
+            };
+
+            alignment = new Dictionary<bool, HorizontalAlignment>()
+            {
+                { false, HorizontalAlignment.Left},
+                { true, HorizontalAlignment.Right}
+            };
+        }
+        //----------------------------------------------------------------------------
+        void listFill(string str)
+        {
+            collection.Add(new ListBoxItem()
+            {
+                HorizontalAlignment = alignment[rightLeft],
+                IsTabStop = false,
+
+                Content = new Border()
+                {
+                    BorderThickness = new Thickness(2),
+                    CornerRadius = new CornerRadius(50),
+
+                    Child = new TextBlock
+                    {
+                        Background = color[rightLeft],
+                        Text = ' ' + str + ' ',
+                        TextWrapping = TextWrapping.Wrap,
+                        FontSize = 15,
+                        FontFamily = new FontFamily("Segoe Print")
+                    }
+                }
+            });
+
+            lbMessages.ScrollIntoView(lbMessages.Items.Cast<ListBoxItem>().Last());
+
+            rightLeft = !rightLeft;
+
+            tbUser.Clear();
         }
         //----------------------------------------------------------------------------
         void SendMessage()
         {
             if (tbUser.Text != String.Empty)
-            {
-                collection.Add(new ListBoxItem()
-                {
-                    Content = new Border()
-                    {
-                        BorderThickness = new Thickness(2),
-                        CornerRadius = new CornerRadius(25),
-                        Child = new TextBlock
-                        {
-                            Text = tbUser.Text,
-                            TextWrapping = TextWrapping.Wrap
-                        }
-                    }
-                });
-
-                lbMessages.Items.Add(collection[collection.Count - 1]);
-
-                lbMessages.ScrollIntoView(lbMessages.Items.Cast<ListBoxItem>().Last());
-                tbUser.Clear();
-            }
+                listFill(tbUser.Text);
         }
         //----------------------------------------------------------------------------
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
@@ -63,6 +95,33 @@ namespace Chat
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SendMessage();
+        }
+        //----------------------------------------------------------------------------
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int n = Int16.Parse(sr.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                    listFill(sr.ReadLine());
+            }
+        }
+        //----------------------------------------------------------------------------
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (collection.Count != 0)
+            {
+                using (StreamWriter sw = new StreamWriter(path, false))
+                {
+                    sw.WriteLine(collection.Count + 1);
+
+                    foreach (ListBoxItem item in collection)
+                    {
+                        sw.WriteLine(item.ContentStringFormat);
+                    }
+                }
+            }
         }
     }
 }
